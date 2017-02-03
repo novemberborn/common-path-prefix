821104304946ac15f41bc30c94e8a0f90e63713b
Hans Klunder <hans.klunder@xs4all.nl>
2017-02-03 20:50:59 +0100

    Backported fix for old versions of node

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+'use strict'
+
 module.exports = function commonPathPrefix (paths, sep) {
   if (!sep) {
-    const m = /(\/|\\)/.exec(paths[0])
+    var m = /(\/|\\)/.exec(paths[0])
     // The first path did not contain any directory components. Bail now.
     if (!m) return ''
     sep = m[0]
@@ -9,23 +11,18 @@ module.exports = function commonPathPrefix (paths, sep) {
   // unicode is a bit of a special beast, so instead of charAt we will
   // need to create an array of code points first
   // https://ponyfoo.com/articles/es6-strings-and-unicode-in-depth
-  const prefix = [...paths[0]]
+  var prefix = paths[0].split(sep)
   var maxLength = prefix.length
   for (var i = 1; i < paths.length; i++) {
-    const matchArray = [...paths[i]]
-    var lastSep = -1
+    var matchArray = paths[i].split(sep)
     for (var j = 0; j < maxLength; j++) {
-      const matchChar = matchArray[j]
-      if (matchChar === sep) {
-        lastSep = j
-      }
-      if (matchChar !== prefix[j]) {
-        maxLength = lastSep
+      if (matchArray[j] !== prefix[j]) {
+        maxLength = j
       }
       if (maxLength < 0) {
         return ''
       }
     }
   }
-  return prefix.slice(0, maxLength + 1).join('')
+  return (prefix.slice(0, maxLength).join(sep)) + ((maxLength > 0) ? sep : '')
 }
