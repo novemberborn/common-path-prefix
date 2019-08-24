dc192e5566e26f52d3167566328fd9442da9b33a
Mark Wubben <mark@novemberborn.net>
2019-08-24 16:05:04 +0200

    Refactor function
    Modern JavaScript, remove seemingly irrelevant Unicode comment, hopefully better variable naming.

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 'use strict'
 
-module.exports = function commonPathPrefix (paths, sep) {
+module.exports = function commonPathPrefix ([first, ...paths], sep) {
   if (!sep) {
-    const m = /(\/|\\)/.exec(paths[0])
-    // The first path did not contain any directory components. Bail now.
-    if (!m) return ''
+    const m = /(\/|\\)/.exec(first)
+    if (!m) return '' // The first path did not contain any directory components. Bail now.
+
     sep = m[0]
   }
 
-  // unicode is a bit of a special beast, so instead of charAt we will
-  // need to create an array of code points first
-  // https://ponyfoo.com/articles/es6-strings-and-unicode-in-depth
-  const prefix = paths[0].split(sep)
-  let maxLength = prefix.length
+  const parts = first.split(sep)
 
-  for (let i = 1; i < paths.length; i++) {
-    const matchArray = paths[i].split(sep)
-    for (let j = 0; j < maxLength; j++) {
-      if (matchArray[j] !== prefix[j]) {
-        maxLength = j
-      }
-      if (maxLength === 0) {
-        return ''
+  let prefix = parts.length
+  for (const p of paths) {
+    const compare = p.split(sep)
+    for (let i = 0; i < prefix; i++) {
+      if (compare[i] !== parts[i]) {
+        prefix = i
       }
     }
+
+    if (prefix === 0) break
   }
-  return prefix.slice(0, maxLength).join(sep) + (maxLength > 0 ? sep : '')
+
+  return prefix === 0 ? '' : parts.slice(0, prefix).join(sep) + sep
 }
