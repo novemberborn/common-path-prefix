a9630edaf794f707d0916969c8562571151da5b1
Mark Wubben <mark@novemberborn.net>
2019-11-03 17:10:39 +0100

    Improve separator detection
    Look for a separator in each path, then fall back to the
    platform-default. Fixes #32.

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
+import path from 'path'
 import test from 'ava'
 import commonPathPrefix from '.'
 
 test('returns an empty string if there is no common prefix', t => {
+  t.is(commonPathPrefix([]), '')
   t.is(commonPathPrefix(['foo', 'bar']), '')
+  t.is(commonPathPrefix(['', '']), '')
 })
 
 test('returns the longest common prefix', t => {
+  t.is(commonPathPrefix(['/foo']), '/foo/')
   t.is(commonPathPrefix(['/foo/bar/baz/qux', '/foo/bar/baz/quux']), '/foo/bar/baz/')
 })
 
@@ -14,10 +18,15 @@ test('supports relative paths', t => {
   t.is(commonPathPrefix(['foo/bar/baz/qux', 'foo/bar/baz/quux']), 'foo/bar/baz/')
 })
 
-test('the file separator is determined by the first occurence of / or \\ in the first path', t => {
+test('the file separator is determined by the first occurence of / or \\ in any path', t => {
   t.is(commonPathPrefix(['/foo/bar', '/baz/qux']), '/')
   t.is(commonPathPrefix(['\\foo\\bar', '\\baz\\qux']), '\\')
   t.is(commonPathPrefix(['/foo/bar', '\\baz\\qux']), '')
+  t.is(commonPathPrefix(['foo', 'foo/']), 'foo/')
+})
+
+test('when no path contains a file separator, the platform default is used', t => {
+  t.is(commonPathPrefix(['foo', 'foo']), `foo${path.sep}`)
 })
 
 test('the file separator can be specified', t => {
